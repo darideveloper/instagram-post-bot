60804dd1c2aaa459571555c86e7e960b6f9e1745
darideveloper <darideveloper@gmail.com>
2023-05-23 13:05:59 -0600

    feat (bot): login with cookies

--- bot.py
+++ bot.py
@@ -1,4 +1,31 @@
+import os
+import json
+from scraping.web_scraping import WebScraping
 
+CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-class Bot ():
+class Bot (WebScraping):
     
+    def __init__ (self):
+        
+        # Variables
+        pages = {
+            "login": "https://www.instagram.com/accounts/login/",
+        }
+        
+        # Start browser
+        print ("login...")
+        cookies = self.__get_cookies__ ()
+        super().__init__ ()
+        self.set_page (pages["login"])
+        self.set_cookies (cookies)
+        
+        input ("end?")
+        
+    def __get_cookies__ (self):
+        with open (os.path.join (CURRENT_FOLDER, "cookies.json")) as file:
+            cookies = json.load (file)
+        return cookies
+    
+if __name__ == "__main__":
+    bot = Bot ()
